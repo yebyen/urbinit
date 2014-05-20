822142b63833ea3ad30adc41dbdbaf81ac8321be
Kingdon Barrett <kbarrett@metrixmatrix.com>
2014-05-20 09:55:58 -0400

    baseimage-docker:i386 -> yebyen/urbinit:i686
    baseimage-docker is from kingdonb/baseimage-docker:i386

--- durbdate.sh
+++ durbdate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd ~/urbit \
-  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:base urbinit/urbinit-base \
-  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:src urbinit/urbinit-src   \
-  && sudo docker.io build --no-cache --rm -t yebyen/urbinit urbinit/                  \
+  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:base-i686 urbinit/urbinit-base \
+  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:src-i686 urbinit/urbinit-src   \
+  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:i686 urbinit/                  \
   && sudo docker.io push yebyen/urbinit
