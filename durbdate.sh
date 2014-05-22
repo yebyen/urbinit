fb340dc2de3fefcaeffe8f3be5225f7da4508ae9
Kingdon Barrett <kbarrett@metrixmatrix.com>
2014-05-22 12:45:05 -0400

    Merge changes from sipsup-dopdep (i686 branch)
    magsup-sogdep is the amd64 branch, based on yebyen/baseimage:amd64

--- durbdate.sh
+++ durbdate.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#cd ~/bin \
   echo "Rebuilding urbinit-src" \
-  && docker build --no-cache --rm -t yebyen/urbinit:src-i686 urbinit-src \
-  && docker build --rm -t yebyen/urbinit:i686 . \
+  && docker build --no-cache --rm -t yebyen/urbinit:src  urbinit-src  \
+  && docker build --rm -t yebyen/urbinit:amd64 . \
   && docker push yebyen/urbinit
