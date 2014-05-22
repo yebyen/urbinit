dd11a04d6822e21074cb124b474806a6c1b6987f
Kingdon Barrett <kbarrett@metrixmatrix.com>
2014-05-22 17:01:34 +0000

    for building i686 branch on CoreOS
    (with the urbinit instructions placing the clone of urbinit.git in ~/bin/
    assume you are running as a privileged user with docker

--- durbdate.sh
+++ durbdate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cd ~/urbit \
-  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:base-i686 urbinit/urbinit-base \
-  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:src-i686 urbinit/urbinit-src   \
-  && sudo docker.io build --no-cache --rm -t yebyen/urbinit:i686 urbinit/                  \
-  && sudo docker.io push yebyen/urbinit
+cd ~/bin \
+  && docker build --no-cache --rm -t yebyen/urbinit:base-i686 urbinit-base \
+  && docker build --no-cache --rm -t yebyen/urbinit:src-i686  urbinit-src  \
+  && docker build --no-cache --rm -t yebyen/urbinit:i686      ./           \
+  && docker push yebyen/urbinit
