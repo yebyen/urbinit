552ca70dbca476f377ccbcb0f997b70b644d1e72
Kingdon Barrett <yebyen@gmail.com>
2014-05-22 20:22:09 -0400

    just to pull && make

--- durbdate-mini.sh
+++ durbdate-mini.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-cd ~/bin \
+#cd ~/bin \
+  echo "Rebuilding i686" \
   && docker build --no-cache --rm -t yebyen/urbinit:i686      ./           \
   && docker push yebyen/urbinit
