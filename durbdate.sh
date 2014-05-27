b7625b2ce02c244d0a75010fe48e984f1d53d9c8
Kingdon Barrett <yebyen@gmail.com>
2014-05-27 19:01:45 -0400

    separately pushing new builds of base and src
    today i686 and base-i686 got rebuilt and pushed
    src-i686 stayed the same and my branch kingdonb/baseimage-docker also
    did not get any rebuild
    
    my main development machines are 32-bit only hosts so this branch should
    be getting more attention than the 64-bit branches, at least until I get
    a maintainer for those branches!  Use either branch, if you are 64-bit.

--- durbdate.sh
+++ durbdate.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-cd ~/bin \
+#cd ~/bin \
+  echo "Rebuilding urbinit-base" \
   && docker build --no-cache --rm -t yebyen/urbinit:base-i686 urbinit-base \
-  && docker build --no-cache --rm -t yebyen/urbinit:src-i686  urbinit-src  \
-  && docker build --no-cache --rm -t yebyen/urbinit:i686      ./           \
   && docker push yebyen/urbinit
