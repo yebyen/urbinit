7c01ad23a454ca2d76defb63c7bd4ece206ca1f7
Kingdon Barrett <yebyen@gmail.com>
2014-05-27 19:25:40 -0400

    freshen cache
    build problems on i686 - could not complete build successfully without
    taking advantage of in-between cache hits - arbitrary steps sometimes
    just randomly failed?  it seemed to help by removing --no-cache

--- durbdate.sh
+++ durbdate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #cd ~/bin \
   echo "Rebuilding urbinit-base" \
-  && docker build --no-cache --rm -t yebyen/urbinit:base-i686 urbinit-base \
+  && docker build --rm -t yebyen/urbinit:base-i686 urbinit-base \
   && docker push yebyen/urbinit
