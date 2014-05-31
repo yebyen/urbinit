80b99250f73730fbdbc161e0992dc008aac300ce
Kingdon Barrett <yebyen@gmail.com>
2014-05-31 00:39:39 -0400

    Rebuilding for continuity breach and flag day

--- durbdate.sh
+++ durbdate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #cd ~/bin \
-  echo "Rebuilding urbinit-base" \
-  && docker build --rm -t yebyen/urbinit:base-i686 urbinit-base \
+  echo "Rebuilding urbinit-src" \
+  && docker build --rm -t yebyen/urbinit:src-i686 urbinit-src \
+  && docker build --rm -t yebyen/urbinit:i686 . \
   && docker push yebyen/urbinit
