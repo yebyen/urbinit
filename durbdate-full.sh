dfff3f98990e255c1105d93eeab0e5898c64aab4
Kingdon <yebyen@gmail.com>
2014-06-15 20:17:20 -0400

    only refresh base-i686

--- durbdate-full.sh
+++ durbdate-full.sh
@@ -2,6 +2,6 @@
 #cd ~/bin \
   echo "Rebuilding urbinit-src" \
   && docker build --rm -t yebyen/urbinit:base-i686 urbinit-base \
-  && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
-  && docker build --rm -t yebyen/urbinit:i686 . \
+  #&& docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
+  #&& docker build --rm -t yebyen/urbinit:i686 . \
   && docker push yebyen/urbinit
