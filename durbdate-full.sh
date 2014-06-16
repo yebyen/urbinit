7d5774de1712520f95b381ae37ae7d23fcd29159
Kingdon <yebyen@gmail.com>
2014-06-15 20:14:25 -0400

    fresh urbinit images pushed today

---
+++ durbdate-full.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#cd ~/bin \
+  echo "Rebuilding urbinit-src" \
+  && docker build --rm -t yebyen/urbinit:base-i686 urbinit-base \
+  && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
+  && docker build --rm -t yebyen/urbinit:i686 . \
+  && docker push yebyen/urbinit
