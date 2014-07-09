194edde5f48099d9a24ceb1d45299b34e71a56a7
Kingdon Barrett <yebyen@gmail.com>
2014-07-09 10:06:32 -0400

    small update - new urbit.pill on github
    non-breaching updates are "small" of course some may require :reset

--- durbdate-mini.sh
+++ durbdate-mini.sh
@@ -2,4 +2,5 @@
 #cd ~/bin \
   echo "Rebuilding i686" \
   && docker build --no-cache --rm -t yebyen/urbinit:i686      ./           \
+  && docker tag yebyen/urbinit:i686 yebyen/urbinit \
   && docker push yebyen/urbinit
