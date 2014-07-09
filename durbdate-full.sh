a4c22935add4a3819594a52edc52efcb704f7260
Kingdon Barrett <yebyen@gmail.com>
2014-07-08 22:06:42 -0400

    i686 rebuild day -- continuity breach
    rebuild from scratch, all urbinit images

--- durbdate-full.sh
+++ durbdate-full.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #cd ~/bin \
-  echo "Rebuilding urbinit-src" \
+  echo "Rebuilding urbinit-base and urbinit-src first" \
   && docker build --rm -t yebyen/urbinit:base-i686 urbinit-base \
   && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
   && docker build --rm -t yebyen/urbinit:i686 . \
