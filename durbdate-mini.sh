70bf1abb1f56f7e14cf38cd9a4ded2454129b35d
Kingdon Barrett <kbarrett@metrixmatrix.com>
2014-07-10 13:06:52 -0400

    catch up amd64 branch to sipsup-dopdep (i686)

--- durbdate-mini.sh
+++ durbdate-mini.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
   echo "Rebuilding amd64" \
+<<<<<<< HEAD
   && docker build --no-cache --rm -t yebyen/urbinit:amd64 . \
   && docker tag yebyen/urbinit:amd64 yebyen/urbinit \
+=======
+  && docker build --no-cache --rm -t yebyen/urbinit:amd64     ./           \
+>>>>>>> catch up amd64 branch to sipsup-dopdep (i686)
   && docker push yebyen/urbinit
