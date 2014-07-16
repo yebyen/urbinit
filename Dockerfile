1e7e31e85f7fcea4a899647510e1d5def24f3cff
Kingdon Barrett <yebyen@gmail.com>
2014-07-16 15:47:43 -0400

    makefile is not -j safe

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM yebyen/urbinit:src-i686
-RUN cd /urbit && git pull && make clean && make distclean all -j3
+RUN cd /urbit && git pull && make clean && make distclean all
 ADD ./_urbinit /.urbinit
 RUN ln -s /root/.urbit /.urbit && mkdir /root/.urbit
 RUN echo 'source $HOME/.profile' >/.bashrc && echo 'export URBIT_HOME=/urbit/urb' >/.profile && echo 'LANG=en_US.UTF-8' > /etc/default/locale
--- durbdate-full.sh
+++ durbdate-full.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #cd ~/bin \
   echo "Rebuilding urbinit-base and urbinit-src first" \
-  && docker build --rm -t yebyen/urbinit:base-i686 urbinit-base \
+  && docker build --rm --no-cache -t yebyen/urbinit:base-i686 urbinit-base \
   && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
   && docker build --rm -t yebyen/urbinit:i686 . \
   && docker tag yebyen/urbinit:i686 yebyen/urbinit \
