9bed1b6967f26dc8b1f4da8bd873aabca73368d5
Kingdon Barrett <yebyen@gmail.com>
2014-07-16 15:47:43 -0400

    makefile is not -j safe

--- durbdate-full.sh
+++ durbdate-full.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
   echo "Rebuilding urbinit-base and urbinit-src first" \
-  && docker build --rm --no-cache -t yebyen/urbinit:base urbinit-base \
-  && docker build --rm -t yebyen/urbinit:src  urbinit-src \
-  && docker build --rm -t yebyen/urbinit:amd64 . \
-  && docker tag yebyen/urbinit:amd64 yebyen/urbinit \
+  && docker build --rm --no-cache -t yebyen/urbinit:base-i686 urbinit-base \
+  && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
+  && docker build --rm -t yebyen/urbinit:i686 . \
+  && docker tag yebyen/urbinit:i686 yebyen/urbinit \
   && docker push yebyen/urbinit
