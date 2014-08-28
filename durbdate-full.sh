7f0954997ce8e1d6f2519fbe1498db751685cebe
Kingdon Barrett <yebyen@gmail.com>
2014-08-28 09:21:05 -0400

    new updating routines

--- durbdate-full.sh
+++ durbdate-full.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
   echo "Rebuilding urbinit-base and urbinit-src first" \
   && docker build --rm --no-cache -t yebyen/urbinit:base-i686 urbinit-base \
-  && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src
+  && docker build --rm -t yebyen/urbinit:src-i686  urbinit-src \
+  && docker run -i -t --name src-i686 yebyen/urbinit:src-i686  true  \
+  && docker export src-i686|docker import - yebyen/urbinit:src-i686  \
+  && docker rm src-i686 && docker push yebyen/urbinit:src-i686
--- durbdate-mini.sh
+++ durbdate-mini.sh
@@ -2,4 +2,5 @@
   echo "Rebuilding i686" \
   && docker build --no-cache --rm -t yebyen/urbinit:i686 . \
   && docker tag yebyen/urbinit:i686 yebyen/urbinit \
-  && docker push yebyen/urbinit
+  && docker push yebyen/urbinit:i686 \
+  && docker push yebyen/urbinit:latest
