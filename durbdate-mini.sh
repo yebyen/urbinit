ece7b05137bf2178cfa9646c2dd93cc30ec4aca3
Kingdon Barrett <yebyen@gmail.com>
2015-06-29 16:52:01 -0400

    docker tag -f

--- durbdate-mini.sh
+++ durbdate-mini.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
   echo "Rebuilding i686" \
   && docker build --no-cache --rm -t yebyen/urbinit:i686 . \
-  && docker tag yebyen/urbinit:i686 yebyen/urbinit \
+  && docker tag -f yebyen/urbinit:i686 yebyen/urbinit \
   && docker push yebyen/urbinit:i686 \
   && docker push yebyen/urbinit:latest
